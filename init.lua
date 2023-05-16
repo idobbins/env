73d46d52c9f96197f556c76832bfaf5e211adee9
idobbins <isaac.dobbins@icloud.com>
2023-05-16 09:51:23 -0600

    sync

--- init.lua
+++ init.lua
@@ -1,8 +1,11 @@
 
+
 -- init.lua
 
 
 -- general
+
+vim.g.mapleader = " "
 
 vim.o.syntax = 'on'
 vim.o.number = true
@@ -12,86 +15,70 @@ vim.o.tabstop = 2
 vim.o.smartindent = true
 vim.o.autoindent = true
 
+
 -- packer
 
 require('packer').startup(function(use)
 
   use 'wbthomason/packer.nvim'
 
+  use {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.0',
+	  requires = { {'nvim-lua/plenary.nvim'} }
+  }
+
+  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
+
   use 'neovim/nvim-lspconfig'
 
-  use 'hrsh7th/nvim-cmp'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'saadparwaiz1/cmp_luasnip'
-  use 'L3MON4D3/LuaSnip'
+  use {'ms-jpq/coq_nvim', branch = 'coq'}
+  use {'ms-jpq/coq.artifacts', branch = 'artifacts'}
+  use {'ms-jpq/coq.thirdparty', branch = '3p'}
 
-  use 'nvim-treesitter/nvim-treesitter'
+  use({"nvim-treesitter/nvim-treesitter", run = ":TSUpdate"})
+
+  use("tpope/vim-fugitive")
 
   use {'nvim-lualine/lualine.nvim', requires = {'kyazdani42/nvim-web-devicons', opt = true}}
 
   use 'klen/nvim-config-local'
 
+  use 'ggandor/leap.nvim'
+
+  use("eandrju/cellular-automaton.nvim")
+
 end)
 
--- Add additional capabilities supported by nvim-cmp
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+-- telescope
+
+local builtin = require('telescope.builtin')
+
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+require('telescope').setup {}
+require('telescope').load_extension('fzf')
+
+-- lsp
 
 local lspconfig = require('lspconfig')
 
--- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-local servers = { 'clangd' }
+-- Automatically start coq
+vim.g.coq_settings = { auto_start = 'shut-up' }
+
+-- Enable some language servers with the additional completion capabilities offered by coq_nvim
+local servers = { 'clangd', 'r_language_server' }
+
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup {
+  lspconfig[lsp].setup(require('coq').lsp_ensure_capabilities({
     -- on_attach = my_custom_on_attach,
-    capabilities = capabilities,
-  }
+  }))
 end
 
--- luasnip setup
-local luasnip = require 'luasnip'
 
--- nvim-cmp setup
-local cmp = require 'cmp'
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-u>'] = cmp.mapping.scroll_docs(-4), -- Up
-    ['<C-d>'] = cmp.mapping.scroll_docs(4), -- Down
-    -- C-b (back) C-f (forward) for snippet placeholder navigation.
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<CR>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
-  }),
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-  },
-}
-
+-- nvim-config-local
 
 require('config-local').setup {
   -- Default configuration (optional)
@@ -103,15 +90,21 @@ require('config-local').setup {
   lookup_parents = false,
 }
 
+
+-- leap
+
+require('leap').add_default_mappings()
+
 -- treesitter
 
 require'nvim-treesitter.configs'.setup {
-  ensure_installed = { "c", "cpp", "cmake", "lua", "vim", },
+  ensure_installed = {},
   sync_install = false,
-  auto_install = false,
+  auto_install = true,
 
   highlight = {
     enable = true,
+    additional_vim_regex_highlighting = false,
   },
 }
 
