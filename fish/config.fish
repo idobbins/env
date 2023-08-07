35c503834ea81341b850261c4964e620b537bc10
idobbins <isaac.dobbins@icloud.com>
2023-08-07 09:29:08 -0600

    sync changes

---
+++ fish/config.fish
@@ -0,0 +1,14 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_add_path /opt/homebrew/bin
+
+fish_add_path /Users/idobbins/.yarn/bin
+fish_add_path /Users/idobbins/.cargo/bin
+
+eval (brew shellenv)
+
+# comment out this line if using warp
+starship init fish | source
+
