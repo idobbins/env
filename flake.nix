7b0562ed0d2b9687b977c5e10c9cc7d1da8749b3
Isaac Dobbins <isaac.dobbins@icloud.com>
2024-12-13 23:16:00 -0700

    sync

--- flake.nix
+++ flake.nix
@@ -2,18 +2,17 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
-
-  outputs = { self, nixpkgs }: let
-    system = "aarch64-darwin";
-    pkgs = nixpkgs.legacyPackages.${system};
+  outputs = { nixpkgs, ... }: let
+    pkgs = import nixpkgs { system = builtins.currentSystem; };
   in {
-    packages.${system}.default = pkgs.buildEnv {
+    packages.${builtins.currentSystem}.default = pkgs.buildEnv {
       name = "my-packages";
       paths = with pkgs; [
         cmake
         git
         neovim
         ripgrep
+        nushell
       ];
     };
   };
