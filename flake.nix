7b2ee34410ef8ed3adf7871169488c8f71fa084f
Isaac Dobbins <isaac.dobbins@icloud.com>
2024-12-14 15:54:04 -0700

    sync

--- flake.nix
+++ flake.nix
@@ -1,50 +1,66 @@
 {
-  description = "Development environments for different tasks";
+  description = "System and development environments";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { nixpkgs, ... }: 
+  outputs = { nixpkgs, home-manager, ... }: 
   let
     system = "aarch64-darwin";
     pkgs = nixpkgs.legacyPackages.${system};
+    
+    # Define base packages that should be in every shell
+    basePackages = with pkgs; [
+      neovim
+      cmake
+      ripgrep
+    ];
   in {
+    # Development shells for project-specific work
     devShells.${system} = {
       default = pkgs.mkShell {
-        packages = with pkgs; [
-          neovim
-          cmake
-          jq
-        ];
+        packages = basePackages;
       };
 
       python = pkgs.mkShell {
-        packages = with pkgs; [
+        packages = basePackages ++ (with pkgs; [
           python3
           poetry
           black
-          pylint
-        ];
-      };
-
-      nodejs = pkgs.mkShell {
-        packages = with pkgs; [
-          nodejs_20
-          yarn
-          nodePackages.typescript
-          nodePackages.typescript-language-server
-        ];
+        ]);
       };
 
       rust = pkgs.mkShell {
-        packages = with pkgs; [
+        packages = basePackages ++ (with pkgs; [
           rustc
           cargo
           rust-analyzer
-          clippy
-        ];
+        ]);
       };
+    };
+
+    # Home Manager configuration
+    homeConfigurations."idobbins" = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      
+      modules = [{
+        home = {
+          username = "idobbins";
+          homeDirectory = "/Users/idobbins";
+          packages = basePackages;
+          
+          # Don't change these
+          stateVersion = "23.11";
+        };
+        
+        # Let home-manager manage itself
+        programs.home-manager.enable = true;
+      }];
     };
   };
 }
