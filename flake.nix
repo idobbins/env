5ae487848777d0ba21697f40fc2ab783d178d017
Isaac Dobbins <isaac.dobbins@icloud.com>
2024-12-13 23:11:55 -0700

    sync

--- flake.nix
+++ flake.nix
@@ -1,40 +1,20 @@
-i{
+{
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
-  outputs = { nixpkgs, home-manager, ... }: {
-    defaultPackage.aarch64-darwin = 
-      home-manager.defaultPackage.aarch64-darwin;
-
-    homeConfigurations.idobbins = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-      
-      modules = [{
-        home = {
-          username = "idobbins";
-          homeDirectory = "/Users/idobbins";
-          stateVersion = "23.11";
-          packages = with nixpkgs.legacyPackages.aarch64-darwin; [
-            cmake
-            git
-            neovim
-            ripgrep
-            nushell
-          ];
-        };
-
-        programs = {
-          home-manager.enable = true;
-          nushell.enable = true;
-        };
-
-        targets.darwin.enable = true;
-      }];
+  outputs = { self, nixpkgs }: let
+    system = "aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system}.default = pkgs.buildEnv {
+      name = "my-packages";
+      paths = with pkgs; [
+        cmake
+        git
+        neovim
+        ripgrep
+      ];
     };
   };
 }
