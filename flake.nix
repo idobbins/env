6ac53a29fbe190d0204d92b5660534f562ee4dbe
Isaac Dobbins <isaac.dobbins@icloud.com>
2024-12-14 15:44:27 -0700

    sync

--- flake.nix
+++ flake.nix
@@ -1,20 +1,50 @@
 {
-  description = "Minimal flake with development tools";
+  description = "Development environments for different tasks";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs = { nixpkgs, ... }: {
-    nixosConfigurations."aarch64-darwin" = nixpkgs.lib.nixosSystem {
-      system = "aarch64-darwin";
-      modules = [{
-        environment.systemPackages = with nixpkgs.legacyPackages.aarch64-darwin; [
+  outputs = { nixpkgs, ... }: 
+  let
+    system = "aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        packages = with pkgs; [
           neovim
           cmake
           jq
         ];
-      }];
+      };
+
+      python = pkgs.mkShell {
+        packages = with pkgs; [
+          python3
+          poetry
+          black
+          pylint
+        ];
+      };
+
+      nodejs = pkgs.mkShell {
+        packages = with pkgs; [
+          nodejs_20
+          yarn
+          nodePackages.typescript
+          nodePackages.typescript-language-server
+        ];
+      };
+
+      rust = pkgs.mkShell {
+        packages = with pkgs; [
+          rustc
+          cargo
+          rust-analyzer
+          clippy
+        ];
+      };
     };
   };
 }
