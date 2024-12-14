0d97e14861627b807ae82e82277c6f6e3bf1d89e
Isaac Dobbins <isaac.dobbins@icloud.com>
2024-12-14 15:04:46 -0700

    sync

---
+++ macos.sh
@@ -0,0 +1,12 @@
+
+# xcode cli tools
+xcode-select --install
+
+# homebrew
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+source ~/.zshrc
+
+# packages
+brew install cmake git jq ripgrep neovim
+
+
